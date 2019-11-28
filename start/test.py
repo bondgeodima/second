3f813722d1d19d8d868bb2d47a89b76cff5690fe
bondgeodima <bondgeodima@gmail.com>
2019-11-28 19:16:28 +0200

    change out arg

---
+++ start/test.py
@@ -0,0 +1,17 @@
+class Room:
+    def __init__(self, number, size):
+        self.number = number
+        self.size = size
+
+    def out(self, arg):
+        s = self.arg * 50
+        return s
+
+
+def main():
+    r1 = Room(10, 400)
+    return r1
+
+
+if __name__ == "__main__":
+    main()
