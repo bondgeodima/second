116f4d107347e8ed02d15df79c25d2fff3e1d448
bondgeodima <bondgeodima@gmail.com>
2019-11-28 19:27:15 +0200

    class Room

--- start/test.py
+++ start/test.py
@@ -4,21 +4,13 @@ class Room:
         self.size = size
 
     def out(self, arg):
-<<<<<<< HEAD
         s = self.arg * 50
-=======
-        s = self.arg
->>>>>>> first
         return s
 
 
 def main():
     r1 = Room(10, 400)
-<<<<<<< HEAD
     return r1
-=======
-    print(r1)
->>>>>>> first
 
 
 if __name__ == "__main__":
