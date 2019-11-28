5687a2161d11ee6e7887315e63cbc029a7c4b17b
bondgeodima <bondgeodima@gmail.com>
2019-11-28 19:03:38 +0200

    class Room

---
+++ start/test.py
@@ -0,0 +1,17 @@
+class Room:
+    def __init__(self, number, size):
+        self.number = number
+        self.size = size
+
+    def out(self, arg):
+        s = self.arg
+        return s
+
+
+def main():
+    r1 = Room(10, 400)
+    print(r1)
+
+
+if __name__ == "__main__":
+    main()
