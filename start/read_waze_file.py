848c9981158a4e3c57cdd5d64f40325d416d5213
bondgeodima <bondgeodima@gmail.com>
2020-01-03 15:30:59 +0200

    change population

--- start/read_waze_file.py
+++ start/read_waze_file.py
@@ -15,35 +15,34 @@ for filename in os.listdir(dirname):
         time = filename[11:16]
         # print('filename: {}, date: {}, time {}'.format(filename, date, time))
 
-# l = []
         with open('D:/TEMP/_____НИГДе/waze_19_12/'+filename) as json_file:
             annotations = json.load(json_file)
             annotations = annotations['alerts']
 
-    # Add images
+            # Add images
             for a in annotations:
-        # Get the x, y coordinaets of points of the polygons that make up
-        # the outline of each object instance. There are stores in the
-        # shape_attributes (see json format above)
-#        country = a['country']
-#        nThumbsUp = a['nThumbsUp']
-#        magvar = a['magvar']
-#        reportRating = a['reportRating']
-#        confidence = a['confidence']
-#        reliability = a['reliability']
+                # Get the x, y coordinaets of points of the polygons that make up
+                # the outline of each object instance. There are stores in the
+                # shape_attributes (see json format above)
+                # country = a['country']
+                # nThumbsUp = a['nThumbsUp']
+                # magvar = a['magvar']
+                # reportRating = a['reportRating']
+                # confidence = a['confidence']
+                # reliability = a['reliability']
                 type_a = a['type']
                 subtype = a['subtype']
                 location = a['location']
                 uuid = a['uuid']
-        # class_ids = [int(n['class']) for n in objects]
-        # l = l + class_ids
+                # class_ids = [int(n['class']) for n in objects]
+                # l = l + class_ids
 
-        # print ('country: {} nThumbsUp: {} magvar: {} reportRating: {} confidence: {} '
-        #        'reliability: {} type: {} subtype {} location {}'.format(country, nThumbsUp, magvar, reportRating,
-        #                                                                 confidence, reliability, type_a, subtype,
-        #                                                                 location))
+                # print ('country: {} nThumbsUp: {} magvar: {} reportRating: {} confidence: {} '
+                #        'reliability: {} type: {} subtype {} location {}'.format(country, nThumbsUp, magvar, reportRating,
+                #                                                                 confidence, reliability, type_a, subtype,
+                #                                                                 location))
 
-        # print ('type: {} subtype {} X {} Y {}'.format(type_a, subtype, location['x'], location['y']))
+                # print ('type: {} subtype {} X {} Y {}'.format(type_a, subtype, location['x'], location['y']))
 
                 # print('{}; {}; {}; {}; {}; {}'.format(type_a, subtype, location['x'], location['y'], date, time))
                 f.write('{}; {}; {}; {}; {}; {}; {} \r\n'.format(uuid, type_a, subtype, location['x'], location['y'], date, time))
