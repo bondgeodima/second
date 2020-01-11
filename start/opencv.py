50750e39f90a1f25f5c3f96601c59e13de02f5f2
bondgeodima <bondgeodima@gmail.com>
2020-01-11 18:05:08 +0200

    change population

--- start/opencv.py
+++ start/opencv.py
@@ -1,9 +1,9 @@
 import cv2
-vidcap = cv2.VideoCapture('D:/TEMP/_deeplearning/_____prezentation/Traffic counting based on OpenCV.mp4')
+vidcap = cv2.VideoCapture('D:/TEMP/_deeplearning/road_signs/__video/VID_20200111_102822.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-  cv2.imwrite("D:/TEMP/_deeplearning/_____prezentation/out/%05d.jpg" % count, image)     # save frame as JPEG file
+  cv2.imwrite("D:/TEMP/_deeplearning/road_signs/__video/out/%05d.jpg" % count, image)     # save frame as JPEG file
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
