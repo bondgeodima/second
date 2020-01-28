b9348ac9d65e56c0408e97e52e9535709dedc7a0
bondgeodima <bondgeodima@gmail.com>
2020-01-28 17:44:59 +0200

    change population

--- start/opencv.py
+++ start/opencv.py
@@ -1,9 +1,23 @@
 import cv2
-vidcap = cv2.VideoCapture('D:/TEMP/_deeplearning/road_signs/__video/VID_20200111_102822.mp4')
-success,image = vidcap.read()
-count = 0
-while success:
-  cv2.imwrite("D:/TEMP/_deeplearning/road_signs/__video/out/%05d.jpg" % count, image)     # save frame as JPEG file
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
+import os
+
+dirname = 'D:/TEMP/_deeplearning/road_signs/_video_25_01_2020/'
+
+
+for filename in os.listdir(dirname):
+    f_type = filename[-3:]
+    f_file = filename[:-4]
+    if f_type == 'mp4':
+        out_name = 'D:/TEMP/_deeplearning/road_signs/_video_25_01_2020/_out_' + f_file + '/'
+        vidcap = cv2.VideoCapture(dirname + filename)
+        # vidcap = cv2.VideoCapture('D:/TEMP/_deeplearning/road_signs/_video_25_01_2020/VID_20200125_154626.mp4')
+        success,image = vidcap.read()
+        count = 0
+        print("start " + filename)
+        while success:
+            # cv2.imwrite("D:/TEMP/_deeplearning/road_signs/_video_25_01_2020/_out_VID_20200125_154626/%05d.jpg" % count, image)     # save frame as JPEG file
+            cv2.imwrite(out_name + "%05d.jpg" % count, image)  # save frame as JPEG file
+            success,image = vidcap.read()
+            # print('Read a new frame: ', success)
+            count += 1
+        print("finish " + filename)
