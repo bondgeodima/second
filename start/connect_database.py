91326d57d599aaa5e91d0b7eb34369f8b348c604
bondgeodima <bondgeodima@gmail.com>
2020-02-18 17:31:48 +0200

    change population

--- start/connect_database.py
+++ start/connect_database.py
@@ -1,5 +1,4 @@
 import io
-import json
 import psycopg2
 
 dir_in = 'D:/TEMP/agro/file/'
@@ -10,7 +9,6 @@ cursor = conn.cursor()
 cursor.execute('SELECT cadnum, ST_AsGeoJSON(the_geom_tr,3) FROM tmp.ss')
 for row in cursor:
     fff = io.open(dir_in + row[0].replace(":","") + '.json', mode="w", encoding="utf-8")
-    # json.dump(row[1], fff)
     fff.write(row[1])
     print(row[1])
     fff.close()
