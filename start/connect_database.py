5f7a80ab340d73bf53b7deff9eea03263ee96985
bondgeodima <bondgeodima@gmail.com>
2020-02-18 17:30:15 +0200

    change population

---
+++ start/connect_database.py
@@ -0,0 +1,16 @@
+import io
+import json
+import psycopg2
+
+dir_in = 'D:/TEMP/agro/file/'
+
+conn = psycopg2.connect(dbname='dzk', user='postgres',
+                        password='gagra321', host='192.168.33.57')
+cursor = conn.cursor()
+cursor.execute('SELECT cadnum, ST_AsGeoJSON(the_geom_tr,3) FROM tmp.ss')
+for row in cursor:
+    fff = io.open(dir_in + row[0].replace(":","") + '.json', mode="w", encoding="utf-8")
+    # json.dump(row[1], fff)
+    fff.write(row[1])
+    print(row[1])
+    fff.close()
