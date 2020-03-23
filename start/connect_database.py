6152915398ed44a63dfcaf59883c1b9f58788aa6
bondgeodima <bondgeodima@gmail.com>
2020-03-23 12:54:27 +0200

    change population

--- start/connect_database.py
+++ start/connect_database.py
@@ -6,7 +6,7 @@ dir_in = 'D:/TEMP/agro/file/'
 conn = psycopg2.connect(dbname='dzk', user='postgres',
                         password='gagra321', host='192.168.33.57')
 cursor = conn.cursor()
-cursor.execute('SELECT cadnum, ST_AsGeoJSON(the_geom_tr,3) FROM tmp.ss')
+cursor.execute('SELECT cadnum, ST_AsGeoJSON(the_geom_tr,3) FROM mirbank.ss')
 for row in cursor:
     fff = io.open(dir_in + row[0].replace(":","") + '.json', mode="w", encoding="utf-8")
     fff.write(row[1])
