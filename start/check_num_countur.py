157b14f89d09be2892534be210d6973f9964f5ea
bondgeodima <bondgeodima@gmail.com>
2020-04-06 18:04:23 +0300

    change population

--- start/check_num_countur.py
+++ start/check_num_countur.py
@@ -1,6 +1,6 @@
 import json
 
-filename_in = 'E:/signs/val/via_region_data.json'
+filename_in = 'E:/signs/train/via_region_data.json'
 with open(filename_in, "r") as read_file:
     data = json.load(read_file)
 c = 0
@@ -8,9 +8,8 @@ k = 0
 for i in data:
     if data[i]['regions']:
         for n in data[i]['regions']:
-            # data[i]['regions'][n]['region_attributes']={'class': '1'}
+            data[i]['regions'][n]['region_attributes']={'class': '1'}
             # print(i, data[i]['regions'][n]['region_attributes'])
             c = c + 1
-    else:
-        k = k + 1
-print('Number of records: ', c, 'Number of bad: ', k)
+
+print('Number of records: ', c)
