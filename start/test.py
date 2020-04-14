5cca2786f41b903ffd2bb2b86d553518203de9a7
bondgeodima <bondgeodima@gmail.com>
2020-04-14 17:01:16 +0300

    Default Changelist

--- start/test.py
+++ start/test.py
@@ -4,13 +4,16 @@ class Room:
         self.size = size
 
     def out(self, arg):
+        self.arg = arg
         s = self.arg * 50
         return s
 
 
 def main():
     r1 = Room(10, 400)
-    return r1
+    w = r1.out(10)
+    print (w)
+    # return w
 
 
 if __name__ == "__main__":
